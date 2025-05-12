58d201b2b7a6fe6616d99b53fe5116aaf98057db
haipinggong <ghaiping@hotmail.com>
2025-05-12 16:44:19 +1000

    Use main in body

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-// import "./index.css";
 import theme from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import GlobalStyles from "@mui/material/GlobalStyles";
